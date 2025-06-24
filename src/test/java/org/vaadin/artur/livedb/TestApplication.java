5c0f8d9904c5a6323ea49ddcff31451435081abf
Simon Martinelli <simon@martinelli.ch>
2025-06-24 11:53:21 +0200

    Integrate Testcontainers to support PostgreSQL by running the TestApplication

---
+++ src/test/java/org/vaadin/artur/livedb/TestApplication.java
@@ -0,0 +1,11 @@
+package org.vaadin.artur.livedb;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
